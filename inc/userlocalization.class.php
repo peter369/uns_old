58f556554801acf66048dca19550102ffbefd86a
peter369 <30994776+peter369@users.noreply.github.com>
2017-08-16 14:04:02 -0500

    Normalizacion de codigo

--- inc/userlocalization.class.php
+++ inc/userlocalization.class.php
@@ -75,58 +75,58 @@ class PluginUnsUserLocalization extends CommonGLPI
 
 //necesito obtener la cantidad de tickets por tecnico para esto necesito saber el nombre  del tecnico
 
-        $sql2 = "SELECT COUNT(tickets_id)
-FROM glpi_tickets_users T1
-INNER JOIN glpi_tickets T2 ON T1.tickets_id=T2.id
-INNER JOIN glpi_users T3 ON T1.users_id=T3.id
-WHERE T3.name=$user[0] AND T1.type=2 AND T2.is_deleted=0";
+                  $sql2 = "SELECT COUNT(tickets_id)
+          FROM glpi_tickets_users T1
+          INNER JOIN glpi_tickets T2 ON T1.tickets_id=T2.id
+          INNER JOIN glpi_users T3 ON T1.users_id=T3.id
+          WHERE T3.name=$user[0] AND T1.type=2 AND T2.is_deleted=0";
 
-        //print_r($art_array);
-        echo "<html>
-<head>
+                  //print_r($art_array);
+                  echo "<html>
+          <head>
 
-  <title>Google Maps Multiple Markers</title>
-  <script src='http://maps.google.com/maps/api/js?key=AIzaSyDsxqHfVk-F5-2pHE3c1AEBD3Mkr_8MzZg' type='text/javascript'></script>
-</head>
-<body>
-  <div id='map' style='height: 500px; width: 980px; margin:auto;'>
-</div>
-<script type='text/javascript'>
-    var locations = [
-      ['<h3>Bondi Beach', -33.890542, 151.274856, 4],
-      ['<h3>Coogee Beach', -33.923036, 151.259052, 5],
-      ['<h3>$nombre[2] $apellido[2]</h3><b>$fecha[2]', $ubicacion[2], 3],
-      ['<h3>$nombre[1] $apellido[1]</h3><b>$fecha[1]', $ubicacion[1], 2],
-      ['<h3>$nombre[0] $apellido[0]</h3>Aqui va el numero de tickets<br><b>$fecha[0]', $ubicacion[0], 1]
-    ];
+            <title>Google Maps Multiple Markers</title>
+            <script src='http://maps.google.com/maps/api/js?key=AIzaSyDsxqHfVk-F5-2pHE3c1AEBD3Mkr_8MzZg' type='text/javascript'></script>
+          </head>
+          <body>
+            <div id='map' style='height: 500px; width: 980px; margin:auto;'>
+          </div>
+          <script type='text/javascript'>
+              var locations = [
+                ['<h3>Bondi Beach', -33.890542, 151.274856, 4],
+                ['<h3>Coogee Beach', -33.923036, 151.259052, 5],
+                ['<h3>$nombre[2] $apellido[2]</h3><b>$fecha[2]', $ubicacion[2], 3],
+                ['<h3>$nombre[1] $apellido[1]</h3><b>$fecha[1]', $ubicacion[1], 2],
+                ['<h3>$nombre[0] $apellido[0]</h3>Aqui va el numero de tickets<br><b>$fecha[0]', $ubicacion[0], 1]
+              ];
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: $z,
-      center: new google.maps.LatLng($center),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+              var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: $z,
+                center: new google.maps.LatLng($center),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+              });
 
-    var infowindow = new google.maps.InfoWindow();
+              var infowindow = new google.maps.InfoWindow();
 
-    var marker, i;
+              var marker, i;
 
 
-    for (i = 0; i < locations.length; i++) {
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
+              for (i = 0; i < locations.length; i++) {
+                marker = new google.maps.Marker({
+                  position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                  map: map
+                });
 
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    }
-  </script>
-</body>
-</html>";
+                google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                  return function() {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                  }
+                })(marker, i));
+              }
+            </script>
+          </body>
+          </html>";
 
     }
 
