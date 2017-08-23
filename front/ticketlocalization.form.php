3f1d2c2add9eb5171a1d54226ba5fb2eb335b7f8
peter369 <30994776+peter369@users.noreply.github.com>
2017-08-23 13:31:11 -0500

    Eliminado la inclusión de la clase

--- front/ticketlocalization.form.php
+++ front/ticketlocalization.form.php
@@ -1,2 +1,16 @@
-include ("ventas/plugins/uns/inc/ticketlocalization.php");
 
+
+<?php
+
+include "../../../inc/includes.php";
+
+$datoTecnico = new PluginUnsTicketLocalization;
+
+if(isset($_POST['var_tecnico']))
+{
+    $uid = $_POST['var_tecnico'];
+    
+    PluginUnsTicketLocalization::RecibirDatos($_POST['var_tecnico']);
+    Html::back();
+}
+Html::displayErrorAndDie('Lost!');
